695d80c301997c076ecda229fffab3103aeee783
dinos31 <vkrlenkala@gmail.com>
2021-08-25 21:16:49 -0500

    Teacher details update

--- model/teacher.go
+++ model/teacher.go
@@ -5,6 +5,6 @@ type TeacherDetails struct {
 	TID        int64  `json:"id" gorm:"column:tid"`
 	TFirstName string `json:"first_name" gorm:"column:tfirst_name"`
 	TLastName  string `json:"last_name" gorm:"column:tlast_name"`
-	Class      string `json:"father_name" gorm:"column:class"`
-	Subject    string `json:"mother_name" gorm:"column:subject"`
+	Class      string `json:"class" gorm:"column:class"`
+	Subject    string `json:"subject" gorm:"column:subject"`
 }
